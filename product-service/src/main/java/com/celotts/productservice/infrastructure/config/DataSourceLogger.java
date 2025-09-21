4f6fa407c12a93d87a84dbfcadd013e14eaa6ee6
Carlos Lott <celotts@gmail.com>
2025-09-20 20:07:24 -0600

    chore: add file with inf

--- product-service/src/main/java/com/celotts/productservice/infrastructure/config/DataSourceLogger.java
+++ product-service/src/main/java/com/celotts/productservice/infrastructure/config/DataSourceLogger.java
@@ -1,4 +1,51 @@
 package com.celotts.productservice.infrastructure.config;
 
+import com.zaxxer.hikari.HikariDataSource;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+
+@Component
+@RequiredArgsConstructor
 public class DataSourceLogger {
+    private static final Logger log = LoggerFactory.getLogger(DataSourceLogger.class);
+    private final DataSource dataSource;
+
+    @PostConstruct
+    public void logDataSourceInfo() {
+        try (Connection c = dataSource.getConnection()) {
+            DatabaseMetaData md = c.getMetaData();
+            log.info("DB Product Name: {}", md.getDatabaseProductName());
+            log.info("DB Version: {}.{}", md.getDatabaseMajorVersion(), md.getDatabaseMinorVersion());
+            log.info("Driver Name: {}", md.getDriverName());
+            log.info("Driver Version: {}", md.getDriverVersion());
+            log.info("AutoCommit: {}", c.getAutoCommit());
+            log.info("Tx Isolation: {}", isolationName(c.getTransactionIsolation()));
+
+            if (dataSource instanceof HikariDataSource hikari) {
+                log.info("Hikari pool: {}", hikari.getPoolName());
+                log.info("Min Idle: {}", hikari.getMinimumIdle());
+                log.info("Max Pool Size: {}", hikari.getMaximumPoolSize());
+            }
+        } catch (Exception e) {
+            log.warn("Could not log datasource info", e);
+        }
+    }
+
+    private String isolationName(int level) {
+        return switch (level) {
+            case Connection.TRANSACTION_NONE -> "NONE";
+            case Connection.TRANSACTION_READ_UNCOMMITTED -> "READ_UNCOMMITTED";
+            case Connection.TRANSACTION_READ_COMMITTED -> "READ_COMMITTED";
+            case Connection.TRANSACTION_REPEATABLE_READ -> "REPEATABLE_READ";
+            case Connection.TRANSACTION_SERIALIZABLE -> "SERIALIZABLE";
+            default -> "UNKNOWN(" + level + ")";
+        };
+    }
 }
