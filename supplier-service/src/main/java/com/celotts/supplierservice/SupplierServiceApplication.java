cbf918ea9853b43b784bde298ffe78a264357069
Carlos Lott <celotts@gmail.com>
2025-10-04 21:54:43 -0600

    feat: Se configura el ambiente

--- supplier-service/src/main/java/com/celotts/supplierservice/SupplierServiceApplication.java
+++ supplier-service/src/main/java/com/celotts/supplierservice/SupplierServiceApplication.java
@@ -2,8 +2,18 @@ package com.celotts.supplierservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@EnableDiscoveryClient
+@EnableJpaRepositories(basePackages = {
+        "com.celotts.supplierservice.infrastructure.adapter.output.postgres.repository"
+})
+@EntityScan(basePackages = "com.celotts.supplierservice.infrastructure.adapter.output.postgres.entity")
+@EnableConfigurationProperties
+@SpringBootApplication(scanBasePackages = "com.celotts.supplierservice")
 public class SupplierServiceApplication {
 
     public static void main(String[] args) {
