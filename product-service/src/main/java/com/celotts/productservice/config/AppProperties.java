5793faa3bf70c19f04f2b9f25409816922e51371
Carlos Lott <celotts@gmail.com>
2025-10-10 13:11:51 -0600

    config: update config

--- product-service/src/main/java/com/celotts/productservice/config/AppProperties.java
+++ product-service/src/main/java/com/celotts/productservice/config/AppProperties.java
@@ -1,4 +1,19 @@
-package com.celotts.productservice.infrastructure.config;
+package com.celotts.productservice.config;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "app")
+@Getter @Setter
 public class AppProperties {
+    private Pagination pagination;
+    @Getter @Setter
+    public static class Pagination {
+        private Integer defaultPage;
+        private Integer defaultSize;
+        private Integer maxSize;
+        private String  defaultSort;
+        private String  defaultDirection;
+    }
 }
