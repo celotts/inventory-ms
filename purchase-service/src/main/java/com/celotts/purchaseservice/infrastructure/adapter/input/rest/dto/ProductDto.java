2c9479db2da00b5bc6824b39b26db9c2ce1a6c41
Carlos Lott <celotts@gmail.com>
2026-02-10 13:36:47 -0600

    feat: Integrate Purchase Service with Product Service
    - Purchase Service:
      - Create ProductDto for receiving product details from product-service.
      - Implement ProductClient (Feign) for inter-service communication with product-service.
    - Product Service:
      - Update Swagger @Operation annotation for getInactiveProducts endpoint for better documentation.

---
+++ purchase-service/src/main/java/com/celotts/purchaseservice/infrastructure/adapter/input/rest/dto/ProductDto.java
@@ -0,0 +1,25 @@
+package com.celotts.purchaseservice.infrastructure.adapter.input.rest.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ProductDto {
+    private UUID id;
+    private String code;
+    private String name;
+
+    @JsonProperty("unit_id")
+    private UUID unitId;
+
+}
